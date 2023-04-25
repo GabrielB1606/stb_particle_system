feb0cb277950e930ff533adf99cf3f909581551f
Gabriel Belisario <gabrielbelisariof@gmail.com>
2023-04-25 16:01:48 -0400

    first commit

---
+++ stb_particle_system.h
@@ -0,0 +1,28 @@
+// stb_particle_system.h - v0.0 - OpenGL 4.6+ Simple Particle System - public domain
+//
+// To build this, in one source file that includes this file do
+//      #define STB_INCLUDE_IMPLEMENTATION
+//
+// DESCIPTION [to do]
+//
+// Options:
+//
+//      [to do]
+//
+// Standard libraries:
+//
+//      stdio.h     FILE, fopen, fclose, fseek, ftell
+//      stdlib.h    malloc, realloc, free
+//      string.h    strcpy, strncmp, memcpy
+//
+// Credits:
+//
+// Written by Gabriel Belisario
+
+#ifndef STB_PARTICLE_SYSTEM_H
+#define STB_PARTICLE_SYSTEM_H
+
+#endif // Header
+
+#ifdef STB_PARTICLE_SYSTEM_IMPLEMENTATION
+#endif // Implementation
