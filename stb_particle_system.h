181e867d6493a275b4a36ee0727ea1ea51e5722a
Gabriel Belisario <gabrielbelisariof@gmail.com>
2023-05-01 10:11:19 -0400

    init quad in constructor

--- stb_particle_system.h
+++ stb_particle_system.h
@@ -27,7 +27,7 @@
 #define STB_PARTICLE_SYSTEM_DEV
 
 #ifdef STB_PARTICLE_SYSTEM_DEV
-#define STB_PARTICLE_SYSTEM_IMPLEMENTATION
+// #define STB_PARTICLE_SYSTEM_IMPLEMENTATION
 #include "headers.h"
 #endif
 
@@ -40,18 +40,18 @@
 
 // All atributes that can be asigned to the particle system from software
 struct ParticleProps{
-    glm::vec2 position; // where the particles will be generated
-    glm::vec2 velocity, velocity_variation; // rate which the particles will be generated
+    glm::vec3 position; // where the particles will be generated
+    glm::vec3 velocity, velocity_variation; // rate which the particles will be generated
     glm::vec4 color_begin, color_end; // color of the particle
     float size_begin, size_end, size_variation; // size of the particle
-    float life_time = 1.f; // how long should a particle be render
+    float life_time = 10.f; // how long should a particle be render
 };
 
 class ParticleSystem{
 
     struct Particle{
-        glm::vec2 position;
-        glm::vec2 velocity;
+        glm::vec3 position;
+        glm::vec3 velocity;
         glm::vec4 color_begin, color_end;
         float rotation = 0.f;
         float size_begin, size_end;
@@ -65,7 +65,15 @@ class ParticleSystem{
     std::vector<Particle> particle_pool;
     uint32_t pool_index = 999;
 
-    unsigned int quad_VA = 0;
+    unsigned int quad_VA = 0, quad_VB = 0, quad_IB = 0;
+    unsigned int indices[6] = { 2, 1, 0, 2, 0, 3 };
+    float vertices[12] = {
+       -1.f, -1.f, 0.0f,
+        1.f, -1.f, 0.0f,
+        1.f,  1.f, 0.0f,
+       -1.f,  1.f, 0.0f
+    };
+
     unsigned int transform_uniform_loc, color_uniform_loc, projview_uniform_loc;
 
 public:
@@ -83,6 +91,20 @@ public:
 
 ParticleSystem::ParticleSystem(){
     particle_pool.resize(1000);
+
+    glCreateVertexArrays(1, &quad_VA);
+    glBindVertexArray(quad_VA);
+
+    glCreateBuffers(1, &quad_VB);
+    glBindBuffer(GL_ARRAY_BUFFER, quad_VB);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glEnableVertexArrayAttrib(quad_VB, 0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+
+    glCreateBuffers(1, &quad_IB);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, quad_IB);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 }
 
 void ParticleSystem::onUpdate(float time_step){
@@ -105,41 +127,12 @@ void ParticleSystem::onUpdate(float time_step){
 
 void ParticleSystem::onRender(unsigned int shader_id, glm::mat4 projection_view_matrix){
 
-    if (!quad_VA){
-
-		float vertices[] = {
-			 -0.5f, -0.5f, 0.0f,
-			  0.5f, -0.5f, 0.0f,
-			  0.5f,  0.5f, 0.0f,
-			 -0.5f,  0.5f, 0.0f
-		};
-
-		glCreateVertexArrays(1, &quad_VA);
-		glBindVertexArray(quad_VA);
-
-		GLuint quadVB, quadIB;
-		glCreateBuffers(1, &quadVB);
-		glBindBuffer(GL_ARRAY_BUFFER, quadVB);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-		glEnableVertexArrayAttrib(quadVB, 0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
-
-		uint32_t indices[] = {
-			0, 1, 2, 2, 3, 0
-		};
-
-		glCreateBuffers(1, &quadIB);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, quadIB);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-		// m_ParticleShader = std::unique_ptr<GLCore::Utils::Shader>(GLCore::Utils::Shader::FromGLSLTextFiles("assets/shader.glsl.vert", "assets/shader.glsl.frag"));
-        projview_uniform_loc = glGetUniformLocation(shader_id, "u_ProjView");
-        transform_uniform_loc = glGetUniformLocation(shader_id, "u_Transform");
-        color_uniform_loc = glGetUniformLocation(shader_id, "u_Color");
-	}
-
     glUseProgram(shader_id);
+
+    projview_uniform_loc = glGetUniformLocation(shader_id, "u_ProjView");
+    transform_uniform_loc = glGetUniformLocation(shader_id, "u_Transform");
+    color_uniform_loc = glGetUniformLocation(shader_id, "u_Color");
+
 	glUniformMatrix4fv(projview_uniform_loc, 1, GL_FALSE, glm::value_ptr(projection_view_matrix));
 
     for (Particle& particle : particle_pool){
@@ -154,9 +147,9 @@ void ParticleSystem::onRender(unsigned int shader_id, glm::mat4 projection_view_
 		float size = glm::lerp(particle.size_end, particle.size_begin, life);
 		
 		// Render
-		glm::mat4 transform = glm::translate(glm::mat4(1.0f), { particle.position.x, particle.position.y, 0.0f })
-			* glm::rotate(glm::mat4(1.0f), particle.rotation, { 0.0f, 0.0f, 1.0f })
-			* glm::scale(glm::mat4(1.0f), { size, size, 1.0f });
+		glm::mat4 transform = glm::translate(glm::mat4(1.0f), particle.position);
+			// * glm::rotate(glm::mat4(1.0f), particle.rotation, { 0.0f, 0.0f, 1.0f })
+			// * glm::scale(glm::mat4(1.0f), { size, size, 1.0f });
 
 		glUniformMatrix4fv(transform_uniform_loc, 1, GL_FALSE, glm::value_ptr(transform));
 		glUniform4fv(color_uniform_loc, 1, glm::value_ptr(color));
@@ -178,6 +171,7 @@ void ParticleSystem::Emit(const ParticleProps &props){
     part.velocity = props.velocity;
     part.velocity.x += props.velocity_variation.x * ( ((float) rand() / RAND_MAX) - 0.5f );
     part.velocity.y += props.velocity_variation.y * ( ((float) rand() / RAND_MAX) - 0.5f );
+    part.velocity.z += props.velocity_variation.z * ( ((float) rand() / RAND_MAX) - 0.5f );
 
     // color
     part.color_begin = props.color_begin;
